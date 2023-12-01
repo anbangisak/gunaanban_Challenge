1e2eaad2280435bd80b95fdf1eb42d1664dd1cdf
Guna Anban G <anbangisak@gmail.com>
2023-12-01 10:39:44 +0530

    https redirect updated

--- server/router.go
+++ server/router.go
@@ -2,13 +2,33 @@ package server
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/unrolled/secure"
 
 	"github.com/anbangisak/gunaanban_Challenge/controllers"
 	"github.com/anbangisak/gunaanban_Challenge/middlewares"
 )
 
 func NewRouter() *gin.Engine {
-	router := gin.New()
+	secureFunc := func() gin.HandlerFunc {
+		return func(c *gin.Context) {
+			secureMiddleware := secure.New(secure.Options{
+				SSLRedirect: true,
+				SSLHost:     "localhost:8888",
+			})
+			err := secureMiddleware.Process(c.Writer, c.Request)
+
+			// If there was an error, do not continue.
+			if err != nil {
+				return
+			}
+
+			c.Next()
+		}
+	}()
+
+	// router := gin.New()
+	router := gin.Default()
+	router.Use(secureFunc)
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 	router.LoadHTMLGlob("./templates/*")
